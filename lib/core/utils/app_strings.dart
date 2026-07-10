2425af77ab804a915a5f59430da0a9a79868aebf
sh2 <tmoawad26@gamil.com>
2026-07-11 00:21:54 +0300

    adding app_strings file

---
+++ lib/core/utils/app_strings.dart
@@ -0,0 +1,45 @@
+abstract class AppStrings {
+  static const exploreUniverseText = 'Explore The Universe';
+  static const explore = 'Explore';
+  // Sun
+  static const sun = 'Sun';
+
+  // Planets (in order from the Sun)
+  static const mercury = 'Mercury';
+  static const venus = 'Venus';
+  static const earth = 'Earth';
+  static const mars = 'Mars';
+  static const jupiter = 'Jupiter';
+  static const saturn = 'Saturn';
+  static const uranus = 'Uranus';
+  static const neptune = 'Neptune';
+  static const sunTitle = 'The Sun: Our Solar System\'s Star';
+  static const sunDescription =
+      """The Sun is the heart of our solar system, a massive ball of 
+      plasma that provides heat, light, and 
+      energy to everything within its gravitational pull. 
+      Its immense size and temperature are fueled by nuclear fusion, 
+      a process that combines hydrogen atoms into helium, releasing vast amounts of energy. 
+      The Sun's magnetic field, which is constantly changing, influences solar activity like 
+      sunspots and solar flares, affecting space weather 
+      and potentially disrupting Earth-based technologies.
+    """;
+  static const mercuryTitle = 'Mercury: The Closest Planet';
+  static const mercuryDesc = """
+        The Sun is the heart of our solar system, a massive ball of plasma that 
+        provides heat, light, and energy to everything within its gravitational pull. 
+        Its immense size and temperature are fueled by nuclear fusion, 
+        a process that combines hydrogen atoms into helium, 
+        releasing vast amounts of energy. The Sun's magnetic field, which is constantly changing,
+         influences solar activity like sunspots and solar flares, 
+         affecting space weather and potentially disrupting Earth-based technologies.
+  """;
+  static const venusTitle = "Venus: Earth's Toxic Twin";
+  static const String venusDesc = """Venus is often referred to as Earth's 
+  twin due to its similar size and composition. However, its thick atmosphere, 
+  composed primarily of carbon dioxide, traps heat, making it the hottest planet in our solar 
+  system. This greenhouse effect has created a hostile environment 
+  with temperatures hot enough to melt lead. Venus is also shrouded in a thick 
+  layer of sulfuric acid clouds, which reflect sunlight and give it a yellowish appearance.
+""";
+}
--- lib/features/splash_screen/splash_screen.dart
+++ lib/features/splash_screen/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:spaceapp/core/utils/app_colors.dart';
 import 'package:spaceapp/core/utils/app_images.dart';
+import 'package:spaceapp/core/utils/app_strings.dart';
 import 'package:spaceapp/core/utils/widgets/explore_button.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -9,26 +10,31 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child:  Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage(AppImages.imagesUniverse))
-            ),
-            child: Align(
-              alignment: Alignment.center,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 17.0),
-                child: Text('Explore The Universe',style: TextStyle(
-                  color: AppColors.white,
-                  fontSize: 48,
-                  fontWeight: .w900,
-                ),),
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(AppImages.imagesUniverse),
+                ),
+              ),
+              child: Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 17.0),
+                  child: Text(
+                    AppStrings.exploreUniverseText,
+                    style: TextStyle(
+                      color: AppColors.white,
+                      fontSize: 48,
+                      fontWeight: .w900,
+                    ),
+                  ),
+                ),
               ),
             ),
-          ),
-          Positioned(
+            Positioned(
               top: 0,
               left: 0,
               right: 0,
@@ -36,17 +42,20 @@ class SplashScreen extends StatelessWidget {
               child: Align(
                 alignment: Alignment.bottomCenter,
                 child: Padding(
-                  padding: const EdgeInsets.only(left: 16.0, right: 17.0,bottom: 16),
+                  padding: const EdgeInsets.only(
+                    left: 16.0,
+                    right: 17.0,
+                    bottom: 16,
+                  ),
                   child: ExploreButton(
-                    onPressed: () {
-                      
-                    }, textButton: 'Explore'),
+                    onPressed: () {},
+                    textButton: AppStrings.explore,
+                  ),
                 ),
               ),
             ),
-            
-        ],
-      ),
+          ],
+        ),
       ),
     );
   }
