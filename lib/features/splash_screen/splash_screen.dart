d31aa18f5b4b598829474af4953d4f9ea134d701
sh2 <tmoawad26@gamil.com>
2026-07-10 23:48:29 +0300

    creating splash screen

--- lib/features/splash_screen/splash_screen.dart
+++ lib/features/splash_screen/splash_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:spaceapp/core/utils/app_colors.dart';
+import 'package:spaceapp/core/utils/app_images.dart';
+import 'package:spaceapp/core/utils/widgets/explore_button.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -6,8 +9,44 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: const Scaffold(
-      
+      child:  Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage(AppImages.imagesUniverse))
+            ),
+            child: Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 17.0),
+                child: Text('Explore The Universe',style: TextStyle(
+                  color: AppColors.white,
+                  fontSize: 48,
+                  fontWeight: .w900,
+                ),),
+              ),
+            ),
+          ),
+          Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 16.0, right: 17.0,bottom: 16),
+                  child: ExploreButton(
+                    onPressed: () {
+                      
+                    }, textButton: 'Explore'),
+                ),
+              ),
+            ),
+            
+        ],
+      ),
       ),
     );
   }
